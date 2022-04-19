fbb156d34055fe20a4e129bb757e0482daa986c3
Sujen Shah <sujen1412@gmail.com>
2022-04-19 08:54:19 -0700

    Use vanilla as base image for jlab

--- jupyterlab3/build-image.sh
+++ jupyterlab3/build-image.sh
@@ -6,7 +6,7 @@ BRANCH=$(basename ${BRANCH})
 pushd ${jupyterlab_dir}
 if [[ -z ${BASE_IMAGE_NAME} ]]; then
     echo "WARNING: No value provided for BASE_IMAGE_NAME, will ./build with default miniconda3 image"
-    BASE_IMAGE_NAME=continuumio/miniconda3:4.10.3p1
+    BASE_IMAGE_NAME=${CI_REGISTRY_IMAGE}/base_images/vanilla:${BRANCH}
 fi
 IMAGE_REF=${CI_REGISTRY_IMAGE}/jupyterlab3/$(basename ${BASE_IMAGE_NAME})
 docker build -t ${IMAGE_REF} --build-arg BASE_IMAGE=${BASE_IMAGE_NAME} -f docker/Dockerfile .
