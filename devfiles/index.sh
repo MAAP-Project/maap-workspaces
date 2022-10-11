c81b002cd2f5c4f4b87aad94ec698c6327123150
Brian Satorius <brian.p.satorius@jpl.nasa.gov>
2022-10-11 13:38:51 -0700

    Reorg devfiles for auto-publishing

---
+++ devfiles/index.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+readarray -d '' metas < <(find devfiles -name 'meta.yaml' -print0)
+N=0
+
+echo [
+
+for meta in "${metas[@]}"; do
+    if [ $N -eq 1 ]
+    then
+      echo ,
+    fi
+    N=$((N+1))
+
+    META_DIR=$(dirname "${meta}")
+    yq r --prettyPrint -j ${META_DIR}/meta.yaml
+done
+
+echo ]
