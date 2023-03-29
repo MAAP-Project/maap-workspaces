86cdb6048ff29bbd7478e85dc3103f4e60c16cbe
Anil Natha <Anil.Natha@jpl.nasa.gov>
2023-03-29 15:17:59 -0700

    Added Cache busting for pangeo base image.

--- base_images/build-image.sh
+++ base_images/build-image.sh
@@ -10,8 +10,9 @@ fi
 for dir in ${DIRS}; do
     pushd $base_image_dir/$dir
     IMAGE_NAME=$(basename $dir)
+    CACHE_BUST=$(date +%s)
     IMAGE_REF=${CI_REGISTRY_IMAGE}/base_images/${IMAGE_NAME}:${BRANCH}
-    docker build -t ${IMAGE_REF} --build-arg IMAGE_REF=${IMAGE_REF} -f docker/Dockerfile .
+    docker build -t ${IMAGE_REF} --build-arg IMAGE_REF=${IMAGE_REF} --build-arg CACHE_BUST=${CACHE_BUST} -f docker/Dockerfile .
     docker push ${IMAGE_REF}
     popd
     echo "$IMAGE_REF" >> built_images.txt
