39f9cacb97a90616d77e7229ecb409270c9528ce
Sujen Shah <sujen1412@gmail.com>
2023-04-12 14:33:50 -0700

    Update CI image build process to allow for custom builds

---
+++ gitlab-ci/generate_configs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+basedir=$( cd "$(dirname "$0")" ; pwd -P )
+git clone https://github.com/MAAP-Project/maap-workspaces.git
+pushd maap-workspaces
+LATEST_COMMIT=$(git log -n 1 --all --format='%h')
+if [[ ! -z ${FORCE_BRANCH_BUILD} ]]; then
+    LATEST_COMMIT=${FORCE_BRANCH_BUILD}
+fi
+git checkout ${LATEST_COMMIT}
+
+if [[ ! -z ${BUILD_ALL_BASE_IMAGES} ]]; then
+    ls -d base_images/*/* > ${basedir}/files_changed.txt
+elif [[ ! -z ${BUILD_SPECIFIC_BASE_IMAGES} ]]; then
+    new_str="${BUILD_SPECIFIC_BASE_IMAGES// /$'\n'}"
+    echo "base_images/${new_str}/docker" > ${basedir}/files_changed.txt
+else
+    # Find files changed in the latest commit
+    echo "BUILD_ALL_BASE_IMAGES and BUILD_SPECIFIC_BASE_IMAGES unset"
+    echo "Getting files changed in last commit"
+    git diff --name-only HEAD HEAD~1 > ${basedir}/files_changed.txt
+fi
+cat ${basedir}/files_changed.txt
+export LATEST_COMMIT
+popd
+rm -rf maap-workspaces
+template="${basedir}/stage.yml.tmpl"
+# For each file changed, check if its in one of the base images directory
+cat ${basedir}/files_changed.txt | while read path
+do
+  if [[ "$path" == base_images/*/* ]]; then
+    second_dir=$(echo "$path" | cut -d'/' -f2)
+    export BASE_IMAGE_TYPE="$second_dir"
+    echo "Adding stage for $path"
+    # Add the base image changed file to build stage for downstream pipeline
+    cat ${template} | CI_JOB_TOKEN='$CI_JOB_TOKEN' CI_REGISTRY='$CI_REGISTRY' envsubst >> stages.yml
+  else
+    echo "Path does not begin with base_images or does not have a second directory, will not do anything"
+  fi
+done
+
+echo "Generate stages.yaml"
+cat stages.yml
