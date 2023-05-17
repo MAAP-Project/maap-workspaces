7f59b408e93cdb002862737c3101e33090fdfd31
Sujen Shah <sujen1412@gmail.com>
2023-05-17 13:48:02 -0700

    Use branch name as tag instead of commit hash

--- gitlab-ci/generate_configs.sh
+++ gitlab-ci/generate_configs.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 set -e
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
+
+# Get webhook payload
+echo TRIGGER_PAYLOAD=$TRIGGER_PAYLOAD
+if [[ ! -z $TRIGGER_PAYLOAD ]]; then
+    PAYLOAD_COMMIT=$(cat $TRIGGER_PAYLOAD | python3 -c "import sys, json; print(json.loads(json.load(sys.stdin)['payload'])['after'])")
+    REF=$(cat $TRIGGER_PAYLOAD | python3 -c "import sys, json; print(json.loads(json.load(sys.stdin)['payload'])['ref'])")
+fi
 git clone https://github.com/MAAP-Project/maap-workspaces.git
 pushd maap-workspaces
-LATEST_COMMIT=$(git log -n 1 --all --format='%h')
+LATEST_COMMIT=$PAYLOAD_COMMIT
+if [[ -z ${PAYLOAD_COMMIT} ]]; then
+    # If no payload commit was set, find the latest commit on the repo
+    LATEST_COMMIT=$(git log -n 1 --all --format='%h')
+fi
 if [[ ! -z ${FORCE_REF_BUILD} ]]; then
     LATEST_COMMIT=${FORCE_REF_BUILD}
 fi
 git checkout ${LATEST_COMMIT}
+
+# Set LATEST_COMMIT to what we want as image tag
+# Default set it to latest commit hash, branch name if commit is HEAD of branch
+LATEST_COMMIT=$(basename $(git symbolic-ref -q --short HEAD || git rev-parse --short HEAD))
+
+# If REF (branch name) in payload use that
+if [[ ! -z ${REF} ]]; then
+    LATEST_COMMIT=$(basename ${REF})
+fi
+
+# If FORCE_CUSTOM_TAGNAME then use that as tag, eg. nightly
+if [[ ! -z ${FORCE_CUSTOM_TAGNAME} ]]; then
+    LATEST_COMMIT=${FORCE_CUSTOM_TAGNAME}
+fi
+echo "Using ${LATEST_COMMIT} as tag for images"
 
 if [[ ! -z ${BUILD_ALL_BASE_IMAGES} ]]; then
     ls -d base_images/*/* > ${basedir}/files_changed.txt
