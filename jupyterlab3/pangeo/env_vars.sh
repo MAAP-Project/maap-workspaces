b8c7ce1060cc7e62a4fddd79422395944aac85cc
Anil Natha <Anil.Natha@jpl.nasa.gov>
2023-08-31 06:57:58 -0700

    Split STAC_CATALOG env var into two separate env vars since the original env var can't be set as a json object.

--- jupyterlab3/pangeo/env_vars.sh
+++ jupyterlab3/pangeo/env_vars.sh
@@ -4,5 +4,6 @@ echo "
 # Pangeo Environment Variables
 export TITILER_STAC_ENDPOINT=https://titiler-stac.maap-project.org/
 export TITILER_ENDPOINT=https://titiler.maap-project.org/
-export STAC_CATALOG='{"name":"MAAP STAC","url":"https://stac.maap-project.org/"}'
+export STAC_CATALOG_NAME="MAAP STAC"
+export STAC_CATALOG_URL="https://stac.maap-project.org/"
 " >> /etc/bash.bashrc
