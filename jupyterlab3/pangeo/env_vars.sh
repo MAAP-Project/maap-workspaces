a33da1229bcb625f1890b5e365e92c4c08df3414
Anil Natha <Anil.Natha@jpl.nasa.gov>
2023-09-01 14:51:06 -0700

    Updated the way env variables are set at the jupyter level via the /etc/environments file instead of /etc/bash.bashrc.

--- jupyterlab3/pangeo/env_vars.sh
+++ jupyterlab3/pangeo/env_vars.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-echo "
-# Pangeo Environment Variables
-export TITILER_STAC_ENDPOINT=https://titiler-stac.maap-project.org/
-export TITILER_ENDPOINT=https://titiler.maap-project.org/
-export STAC_CATALOG_NAME="MAAP STAC"
-export STAC_CATALOG_URL="https://stac.maap-project.org/"
-" >> /etc/bash.bashrc
+# Create temporary file containing our env vars
+echo "TITILER_STAC_ENDPOINT=https://titiler-stac.maap-project.org/
+TITILER_ENDPOINT=https://titiler.maap-project.org/
+STAC_CATALOG_NAME="MAAP STAC"
+STAC_CATALOG_URL="https://stac.maap-project.org/"
+" >> /tmp/pangeo_env_vars
+
+# Add our env vars to system environment
+cat /etc/environment >> /tmp/pangeo_env_vars
+cp /tmp/pangeo_env_vars /etc/environment
