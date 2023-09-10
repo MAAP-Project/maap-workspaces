c42673d776ded357d1cd27bdf1d998b3b30f493d
Anil Natha <Anil.Natha@jpl.nasa.gov>
2023-09-10 16:29:24 -0700

    Reintroducted env var update via adding to /etc/environment file instead of using environment.yml to load variables because the latter was not working as expected.

---
+++ jupyterlab3/pangeo/update-env-vars.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Create temporary file containing our env vars
+echo "TITILER_STAC_ENDPOINT='https://titiler-stac.maap-project.org/'
+TITILER_ENDPOINT='https://titiler.maap-project.org/'
+STAC_CATALOG_NAME='MAAP STAC'
+STAC_CATALOG_URL='https://stac.maap-project.org/'
+" >> /tmp/pangeo_env_vars
+
+# Add our env vars to system environment
+cat /etc/environment >> /tmp/pangeo_env_vars
+cp /tmp/pangeo_env_vars /etc/environment
+rm /tmp/pangeo_env_vars
