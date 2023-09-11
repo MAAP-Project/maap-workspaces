da02718b0ad2d36b416f693b6f192ba74094183c
Anil Natha <Anil.Natha@jpl.nasa.gov>
2023-09-10 19:10:11 -0700

    Testing alternate method to activating conda env

--- jupyterlab3/entrypoint.sh
+++ jupyterlab3/entrypoint.sh
@@ -108,6 +108,7 @@ env | grep _ >> /etc/environment
 export PATH=$PATH:/opt/conda/bin
 cp /root/.bashrc ~/.bash_profile
 conda init
+conda activate base
 
 # Need to fix directory permissions for publickey authentication
 chmod 700 /projects
