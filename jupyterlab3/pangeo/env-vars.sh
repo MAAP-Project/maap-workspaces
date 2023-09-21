a54dbc89554e2a1c848aab255dee614d2aaa84aa
Anil Natha <Anil.Natha@jpl.nasa.gov>
2023-09-21 13:04:55 -0700

    Removed env-vars.sh and cleaned up unused code in jupyter dockerfile.

--- jupyterlab3/pangeo/env-vars.sh
+++
@@ -1,4 +0,0 @@
-export TITILER_STAC_ENDPOINT=https://titiler-stac.maap-project.org/
-export TITILER_ENDPOINT=https://titiler.maap-project.org/
-export STAC_CATALOG_NAME='MAAP STAC'
-export STAC_CATALOG_URL=https://stac.maap-project.org/
