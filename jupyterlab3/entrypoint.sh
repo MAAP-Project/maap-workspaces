9d3c5b1963f3f8b0962f02c52d29f56f0ed3c6cc
Anil Natha <Anil.Natha@jpl.nasa.gov>
2023-12-06 16:06:01 -0800

    Updated notebook lib path and jupyter server lib path to point to the appropriate custom conda environment.

--- jupyterlab3/entrypoint.sh
+++ jupyterlab3/entrypoint.sh
@@ -90,8 +90,8 @@ done
 # allows `base_url` to be set so that HTTP *responses* include the `base_url` so that browsers make the correct
 # call. However, it strips out `base_url` when listening for *requests* so that handles the ingress/nginx proxy
 # requests correctly.
-export NOTEBOOKLIBPATH=$(find /opt/conda/lib/ -maxdepth 3 -type d -name "notebook")
-export JUPYTERSERVERLIBPATH=$(find /opt/conda/lib -maxdepth 3 -type d -name "jupyter_server")
+export NOTEBOOKLIBPATH=$(find /opt/conda/envs/$WORKSPACE_TYPE/lib -maxdepth 3 -type d -name "notebook")
+export JUPYTERSERVERLIBPATH=$(find /opt/conda/envs/$WORKSPACE_TYPE/lib -maxdepth 3 -type d -name "jupyter_server")
 
 read -r -d '' JUPYTER_PATCH << EOM
     # Fix for Tornado's inability to handle proxy requests
@@ -124,7 +124,7 @@ env | grep _ >> /etc/environment
 export PATH=$PATH:/opt/conda/envs/$WORKSPACE_TYPE/bin
 
 # Set up default conda environment for terminals
-echo ". /opt/conda/etc/profile.d/conda.sh ; conda activate ${WORKSPACE_TYPE}" > /etc/profile.d/init_conda.sh
+echo ". /opt/conda/etc/profile.d/conda.sh ; conda activate $WORKSPACE_TYPE" > /etc/profile.d/init_conda.sh
 
 # Need to fix directory permissions for publickey authentication
 chmod 700 /projects
