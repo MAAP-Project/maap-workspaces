7edbac3bffb3367e8dc2678704b23fe9094f3866
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2024-01-02 11:50:34 -0800

    fixed but for Fgmutils install

--- base_images/r/scripts/install_cran_packages_r.sh
+++ base_images/r/scripts/install_cran_packages_r.sh
@@ -7,17 +7,17 @@ export DEBIAN_FRONTEND=noninteractive
 ## build ARGs
 NCPUS=${NCPUS:--1}
 
-Rscript /scripts/install2.r --error --skipmissing --skipinstalled -r "http://cran.us.r-project.org" -n "$NCPUS" nlraa sfarrow
+Rscript /scripts/install2.r --error --skipmissing --skipinstalled -r "http://cran.us.r-project.org" -n "$NCPUS" nlraa sfarrow Fgmutils
 #R -e install.packages('nlraa')
 
 # Clean up
 rm -rf /var/lib/apt/lists/*
-if [ -d '/tmp/downloaded_packages']; then 
+if [ -d '/tmp/downloaded_packages' ]; then 
     rm -r /tmp/downloaded_packages
 fi
 #rm -r /tmp/downloaded_packages
 
-if [ -f '/usr/local/lib/R/site-library/*/libs/*.so']; then
+if [ -f '/usr/local/lib/R/site-library/*/libs/*.so' ]; then
     strip /usr/local/lib/R/site-library/*/libs/*.so
 fi
 #strip /usr/local/lib/R/site-library/*/libs/*.so
