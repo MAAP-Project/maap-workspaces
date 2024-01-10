f30932b1340cf03ba44ead631df2046d3981c728
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2024-01-10 13:46:28 -0800

    added descriptions necessary to use install2.r with GPL license

--- base_images/r/scripts/install2.r
+++ base_images/r/scripts/install2.r
@@ -7,9 +7,31 @@
 # Copyright (C) 2018 - 2023  Carl Boettiger, Dirk Eddelbuettel, Brandon Bertelsen, and SHIMA Tatsuya
 #
 # Released under GPL (>= 2)
+# This file was extracted from this repository https://github.com/rocker-org/rocker-versioned2
+# The license of this repository (rocker-versioned2) can be view here: 
+# https://github.com/rocker-org/rocker-versioned2/blob/master/LICENSE
+
+#    Warranty description we are required to convey: 
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#    Any questions, please contact: grace.llewellyn@jpl.nasa.gov 
+
 
 ## load docopt package from CRAN
-install.packages('docopt', repos = "http://cran.us.r-project.org")
+# Grace Llewellyn added the repos parameter to fix a bug 
+install.packages('docopt', repos = "http://cran.us.r-project.org") 
 library(docopt)
 
 ## default to first library location in .libPaths()
