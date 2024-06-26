d90f07e811b7fee691c5ba2f4c7b5e6c70779198
bsatoriu <27687558+bsatoriu@users.noreply.github.com>
2024-06-26 12:06:51 -0700

    use abs directory for compile file

--- devfiles/devfile_compile.py
+++ devfiles/devfile_compile.py
@@ -7,25 +7,27 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 INDEX_FILE = "index.json"
 META_FILE = "meta.yaml"
 
-with open(INDEX_FILE, "w") as myfile:
-    myfile.write("[")
+# Build the index.json dev registry file
+with open(os.path.join(dir_path, INDEX_FILE), "w") as registry_index_file:
+    registry_index_file.write("[")
 
 i = 0
 
 for subdir, dirs, files in os.walk(dir_path):
     for file in files:
         if file == META_FILE:
+            # Append the devfile metadata to the registry index
             print("Updating " + os.path.join(subdir, file))
 
             if i > 0:
-                with open(INDEX_FILE, "a") as myfile:
-                    myfile.write(", ")
+                with open(os.path.join(dir_path, INDEX_FILE), "a") as registry_index_file:
+                    registry_index_file.write(", ")
 
             i += 1
 
-            with open(os.path.join(subdir, file), 'r') as yaml_in, open(INDEX_FILE, "a") as json_out:
+            with open(os.path.join(subdir, file), 'r') as yaml_in, open(os.path.join(dir_path, INDEX_FILE), "a") as json_out:
                 yaml_object = yaml.safe_load(yaml_in) 
                 json.dump(yaml_object, json_out)
 
-with open(INDEX_FILE, "a") as myfile:
-    myfile.write("]")
+with open(os.path.join(dir_path, INDEX_FILE), "a") as registry_index_file:
+    registry_index_file.write("]")
