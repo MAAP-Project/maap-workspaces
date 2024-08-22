00637adeb9261690acc5edbbcd6b183bc4f11cdb
Sujen Shah <sujen1412@gmail.com>
2024-08-22 11:18:27 -0700

    Add build file for custom images

---
+++ custom_images/build-image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+base_image_dir=$(dirname "$0")
+# Check if on a branch or in a detached HEAD state get commit sha
+BRANCH=$(basename "$(git symbolic-ref -q --short HEAD || git rev-parse --short HEAD)")
+if [ "$#" -eq 0 ]; then
+  DIRS_LIST=( "maap_dps" )
+else
+  DIRS_LIST=( "$@" )
+fi
+for dir in "${DIRS_LIST[@]}"; do
+    pushd "$base_image_dir"/"$dir"
+    IMAGE_NAME=$(basename "$dir")
+    IMAGE_PATH=$(basename "$base_image_dir")
+    IMAGE_REF=${CI_REGISTRY_IMAGE}/${IMAGE_PATH}/${IMAGE_NAME}:${BRANCH}
+#    docker build -t "${IMAGE_REF}" --build-arg IMAGE_REF="${IMAGE_REF}" -f docker/Dockerfile .
+#    docker push "${IMAGE_REF}"
+    popd
+    echo "$IMAGE_REF" >> built_images.txt
+done
+
