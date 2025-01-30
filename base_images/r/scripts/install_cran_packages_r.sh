fe9cc735a2aab4c5594f2c1ae2728c4ef7f209ae
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2025-01-30 14:48:14 -0800

    simplified r base image

--- base_images/r/scripts/install_cran_packages_r.sh
+++ base_images/r/scripts/install_cran_packages_r.sh
@@ -7,7 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 ## build ARGs
 NCPUS=${NCPUS:--1}
 
-Rscript /scripts/install2.r --error --skipmissing --skipinstalled -r "https://r-lidar.r-universe.dev" -n "$NCPUS" lasR
+Rscript /scripts/install2.r --error --skipmissing --skipinstalled -r "https://r-lidar.r-universe.dev" lasR
 Rscript /scripts/install2.r --error --skipmissing --skipinstalled -r "http://cran.us.r-project.org" -n "$NCPUS" nlraa sfarrow tidyterra BIOMASS cforest
 
 # Clean up
