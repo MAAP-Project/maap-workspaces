30a3d1e3e8dd232e765d9a5441170a45895b4481
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2025-01-30 11:28:18 -0800

    updates to maap base to try to install lasR

---
+++ custom_images/maap_base/scripts/install_cran_packages_r.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# always set this for scripts but don't declare as ENV..
+export DEBIAN_FRONTEND=noninteractive
+
+## build ARGs
+NCPUS=${NCPUS:--1}
+
+Rscript /scripts/install2.r --error --skipmissing --skipinstalled -r "https://r-lidar.r-universe.dev" lasR
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+if [ -d '/tmp/downloaded_packages' ]; then 
+    rm -r /tmp/downloaded_packages
+fi
+#rm -r /tmp/downloaded_packages
+
+if [ -f '/usr/local/lib/R/site-library/*/libs/*.so' ]; then
+    strip /usr/local/lib/R/site-library/*/libs/*.so
+fi
+#strip /usr/local/lib/R/site-library/*/libs/*.so
