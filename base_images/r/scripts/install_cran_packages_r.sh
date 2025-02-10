0bf7540ce01bf3f020c792a2f5974c4d8ce5c837
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2025-02-10 14:05:41 -0800

    removed install of BIOMASS and tidyterra and changed cforest to partykit

--- base_images/r/scripts/install_cran_packages_r.sh
+++ base_images/r/scripts/install_cran_packages_r.sh
@@ -7,7 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 ## build ARGs
 NCPUS=${NCPUS:--1}
 
-Rscript /scripts/install2.r --error --skipmissing --skipinstalled -r "http://cran.us.r-project.org" -n "$NCPUS" nlraa sfarrow tidyterra BIOMASS cforest
+Rscript /scripts/install2.r --error --skipmissing --skipinstalled -r "http://cran.us.r-project.org" -n "$NCPUS" nlraa sfarrow partykit
 
 # Clean up
 rm -rf /var/lib/apt/lists/*
