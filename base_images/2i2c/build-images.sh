ab17ff97a1c055573513acef668189288778cc77
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2026-04-15 16:40:34 -0700

    updates from testing build images

--- base_images/2i2c/build-images.sh
+++ base_images/2i2c/build-images.sh
@@ -5,7 +5,7 @@ set -ex
 # Check required parameters
 if [ -z "$ENV" ]; then
     echo "Error: ENV variable is required"
-    echo "Usage: ENV=<environment> TAG=<tag> ./build-images.sh Options are DIT, UAT, OPS"
+    echo "Usage: ENV=<environment> TAG=<tag> ./build-images.sh Options are dit, uat, ops"
     echo "Example: ENV=dit TAG=develop ./build-images.sh"
     exit 1
 fi
@@ -13,7 +13,7 @@ fi
 if [ -z "$TAG" ]; then
     echo "Error: TAG variable is required"
     echo "Usage: ENV=<environment> TAG=<tag> ./build-images.sh DIT images should be tagged as develop and UAT/OPS releases should have numbered tag"
-    echo "Example: ENV=dev TAG=v1.0.0 ./build-images.sh"
+    echo "Example: ENV=dit TAG=develop ./build-images.sh"
     exit 1
 fi
 
@@ -34,13 +34,18 @@ R_BASE_IMAGE_REF="${REGISTRY_BASE}/root/maap-workspaces/2i2c/r-base:${TAG}"
 echo "Building r-base image: ${R_BASE_IMAGE_REF}"
 
 docker build -t ${R_BASE_IMAGE_REF} --build-arg IMAGE_REF=${R_BASE_IMAGE_REF} -f docker/Dockerfile .
-docker push ${R_BASE_IMAGE_REF}
+# manually add this back in
+#docker push ${R_BASE_IMAGE_REF}
 
 popd
 echo "$R_BASE_IMAGE_REF" >> built_images.txt
 
 # Define the images to build
 IMAGE_TYPES="isce3 pangeo r"
+
+DOCKERIMAGE_PATH_DEFAULT="${REGISTRY_BASE}/root/maap-workspaces/2i2c/custom_images/maap_base:${TAG}"
+
+echo "docker image default path $DOCKERIMAGE_PATH_DEFAULT"
 
 for IMAGE_TYPE in ${IMAGE_TYPES}; do
     pushd $base_image_dir/$IMAGE_TYPE
@@ -52,9 +57,9 @@ for IMAGE_TYPE in ${IMAGE_TYPES}; do
 
     # For R image, pass the r-base image as BASE_IMAGE
     if [ "$IMAGE_TYPE" = "r" ]; then
-        docker build -t ${IMAGE_REF} --build-arg IMAGE_REF=${IMAGE_REF} --build-arg BASE_IMAGE=${R_BASE_IMAGE_REF} -f docker/Dockerfile .
+        docker build -t ${IMAGE_REF} --build-arg IMAGE_REF=${IMAGE_REF} --build-arg BASE_IMAGE=${R_BASE_IMAGE_REF} --build-arg DOCKERIMAGE_PATH_DEFAULT=${DOCKERIMAGE_PATH_DEFAULT} -f docker/Dockerfile .
     else
-        docker build -t ${IMAGE_REF} --build-arg IMAGE_REF=${IMAGE_REF} -f docker/Dockerfile .
+        docker build -t ${IMAGE_REF} --build-arg IMAGE_REF=${IMAGE_REF} --build-arg DOCKERIMAGE_PATH_DEFAULT=${DOCKERIMAGE_PATH_DEFAULT} -f docker/Dockerfile .
     fi
 
     # Push the image, commenting this line out as default 
